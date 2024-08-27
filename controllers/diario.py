1d85c62e3e7bbffde021177ad1bd6613c9690243
allysonEscolar <160549266+allysonEscolar@users.noreply.github.com>
2024-08-27 00:46:47 +0000

    criacao das interfaces do CRUD

--- controllers/diario.py
+++ controllers/diario.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash, Blueprint, abort
+from flask import render_template, request, redirect, flash, Blueprint, abort, url_for
 from jinja2 import TemplateNotFound
 from models.database import db
 from models.diario import Diario
@@ -7,7 +7,21 @@ bp_diario = Blueprint("diario", __name__, template_folder='templates')
 
 @bp_diario.route('/recovery')
 def recovery():
+    dados = Diario.query.all()
+ 
     try:
-        return render_template('diario_recovery.html')
+        return render_template('diario_recovery.html', dados = dados)
     except TemplateNotFound:
         abort(404)
+
+@bp_diario.route('/create', methods=['GET', 'POST'])
+def create():
+    if(request.method == 'GET'):
+        return render_template('diario_create.html')
+    if(request.method == 'POST'):
+        titulo = request.form['titulo']
+        disciplina = request.form['disciplina']
+        db.session.add(Diario(titulo, disciplina))
+        db.session.commit()
+        return redirect(url_for('diario.recovery'))
+
